b987194451be380d3c3a0c61c89413dcf06f63e2
Nate Robinson <n4te.robinson@gmail.com>
2016-05-03 18:32:59 +0800

    Add logger

--- crackmes/keygens/keygen.py
+++ crackmes/keygens/keygen.py
@@ -1,10 +1,16 @@
+import logging
+
 class Keygen():
 
     __login      = None
     __serial_key = None
+    __debug      = None
+    __logger     = None
 
-    def __init__ (self, login):
+    def __init__ (self, login, debug=False):
         self.set_login(login)
+        self.set_debug(debug)
+        self.set_logger()
 
     def __call__(self):
         self.generate_key()
@@ -21,6 +27,20 @@ class Keygen():
 
     def get_serial_key(self):
         return self.__serial_key
+
+    def set_debug(self, debug):
+        self.__debug = debug
+
+    def is_debug(self):
+        return self.__debug
+
+    def set_logger(self):
+        logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
+        self.__logger = logging.getLogger()
+
+    def logger(self, message):
+        if self.is_debug():
+            self.__logger.debug(message)
 
     def login_routine (self):
         raise NotImplementedError
