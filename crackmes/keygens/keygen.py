9bbf22165d331858fd177d5e2e7caead1755a552
Nate Robinson <n4te.robinson@gmail.com>
2016-05-03 12:38:39 +0800

    Keygen modified.

--- crackmes/keygens/keygen.py
+++ crackmes/keygens/keygen.py
@@ -1,62 +1,35 @@
-class KeyGen():
-	'''
-	Base class for keygen
-	'''
-	__login               = None
-	__login_routine       = None
-	__serial_key          = None
-	__serial_key_routine  = None
-	__check_routine       = None
-	__generate_serial_key = None
-	
-	def __init__ (self, login, serial_key):
-		self.setLogin(login)
-		self.setSerialKey(serial_key)
-	
-	def setLogin(self, login):
-		self.__login = login
-	
-	def getLogin(self):
-		return self.__login
-	
-	def setLoginRoutine(self, login_routine):
-		self.__login_routine = login_routine
-	
-	def getLoginRoutine(self):
-		return self.__login_routine
-	
-	def setSerialKey(self, serial_key):
-		self.__serial_key = serial_key
-	
-	def getSerialKey(self):
-		return self.__serial_key
-	
-	def setSerialKeyRoutine(self, serial_key_routine):
-		self.__serial_key_routine = serial_key_routine
-	
-	def getSerialKeyRoutine(self):
-		return self.__serial_key_routine
-	
-	def setCheckRoutine(self, check_routine):
-		self.__check_routine = check_routine
-	
-	def getCheckRoutine(self):
-		return self.__check_routine
-	
-	def setGenerateSerialKey(self, generate_serial_key):
-		self.__generate_serial_key = generate_serial_key
-	
-	def getGenerateSerialKey(self):
-		return self.__generate_serial_key
-	
-	def loginRoutine (self):
-		raise NotImplementedError
-	
-	def serialKeyRoutine(self):
-		raise NotImplementedError
-	
-	def checkRoutine(self):
-		raise NotImplementedError
-	
-	def generateSerialKey(self):
-		raise NotImplementedError
+class Keygen():
+
+    __login      = None
+    __serial_key = None
+
+    def __init__ (self, login):
+        self.set_login(login)
+
+    def __call__(self):
+        self.generate_key()
+        self.check_routine()
+
+    def set_login(self, login):
+        self.__login = login
+
+    def get_login(self):
+        return self.__login
+
+    def set_serial_key(self, serial_key):
+        self.__serial_key = serial_key
+
+    def get_serial_key(self):
+        return self.__serial_key
+
+    def login_routine (self):
+        raise NotImplementedError
+
+    def serial_key_routine(self):
+        raise NotImplementedError
+
+    def check_routine(self):
+        raise NotImplementedError
+
+    def generate_key(self):
+        raise NotImplementedError
